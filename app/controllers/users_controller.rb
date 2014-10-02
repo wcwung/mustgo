56fabbea1b4b71818b991138c55826ca6762c819
William Wung <williamcwung@gmail.com>
2014-10-01 17:33:17 -0700

    Signup form error message handles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-    	flash[:success] = "Hello there!"
+    	flash[:success] = "Welcome to mustgo! Find your next favorite destination"
       redirect_to @user
     else
       render 'new'
